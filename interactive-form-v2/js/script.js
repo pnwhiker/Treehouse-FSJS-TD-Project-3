645ed715309ef8dbbb468486f251dc3237ea40bf
PNWHiker <53063554+pnwhiker@users.noreply.github.com>
2020-11-22 14:42:04 -0800

    Updates to Script.js
    Variable declarations completed for shirt size, color and design selection

--- interactive-form-v2/js/script.js
+++ interactive-form-v2/js/script.js
@@ -1,30 +1,25 @@
+// General Purpose Variables Section
+
 const form = document.querySelector('form');
 const body = document.querySelector('body');
-
-
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 
 
-// Page Load Focus = Name Input Element
+// Page Load & Focus Section
 
 body.onload = function (){
     nameInput.focus();
 };
 
-// Job Role Selection = Other = > Text Input Field Vis On/Off
+// Job Role Section
 
 const jobRoleSelect = document.getElementById('title');
-
 const otherJobRoleInput = document.getElementById('other-job-role');
-
-
 otherJobRoleInput.style.display = 'none';
 
 let roleSelection = jobRoleSelect.value;
 console.log(`first role selection is: ${roleSelection}`);
-
-
 
 jobRoleSelect.addEventListener('change', () => {
     roleSelection = jobRoleSelect.value;
@@ -36,4 +31,19 @@ jobRoleSelect.addEventListener('change', () => {
     }   
 
 });
+
+
+// Shirt Size & Design Section
+
+const shirtSizeSelect = document.getElementById('size');
+let shirtSize = shirtSizeSelect.value;
+
+const shirtDesignSelect = document.getElementById('design');
+let shirtDesign = shirtDesignSelect.value;
+
+const shirtColorSelect = document.getElementById('color');
+let shirtColor = shirtColorSelect.value;
+
+
+// Activities Section
 
